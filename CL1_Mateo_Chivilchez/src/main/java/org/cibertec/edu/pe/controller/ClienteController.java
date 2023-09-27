d793445d4c1dbcbf13814eb19040856783761652
MateoChi10 <mateo_chivilchez@hotmail.com>
2023-09-26 19:20:42 -0500

    commit 2

--- CL1_Mateo_Chivilchez/src/main/java/org/cibertec/edu/pe/controller/ClienteController.java
+++ CL1_Mateo_Chivilchez/src/main/java/org/cibertec/edu/pe/controller/ClienteController.java
@@ -7,9 +7,12 @@ import org.cibertec.edu.pe.interfaceService.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class ClienteController {
 
 	@Autowired
@@ -27,13 +30,13 @@ public class ClienteController {
 	//Metodo agregar
 	@GetMapping("/nuevo")
 	public String agregar(Model m) {
-		m.addAttribute("clientes", new cliente());
+		m.addAttribute("cliente", new cliente());
 		return "form";
 	}
 	
 	//Metodo grabar
 	@GetMapping("/guardar")
-	public String guardar(cliente c,Model m) {
+	public String guardar(@Validated cliente c,Model m) {
 		servicio.Grabar(c);
 		return "redirect:/listar";
 	}
